dad90cb21267228d19e86a0501f36685a4ea1a44
Pedro Paulo <90223718+pedro23p@users.noreply.github.com>
2022-10-06 18:00:02 -0300

    Reprodução e arquiterura

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, TextInput ,View, Button  } from 'react-native';
+import * as Speech from 'expo-speech';
 
 export default function App() {
+  const [text, setText] = useState('Bem vindo, Programador')
+
+  function speak(){
+    Speech.speak(text,{
+      language:'pt-BR'
+    });
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+      style={styles.input}
+      onChangeText={e => setText(e)}
+      placeholder="Digite o que deseja reproduzir"
+      />
+
+      <Button
+      title="Falar"
+      onPress={speak}
+      />
     </View>
   );
 }
@@ -17,4 +35,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input:{
+    width:'90%',
+    height: 50,
+    borderWidth:1,
+    borderColor:'#121212',
+    borderRadius:10,
+    padding:10,
+    margin:10,
+    color:'#000',
+    fontSize:18
+  }
 });
